43084b2064f3c8a2b185be62c1e359969c165817
wambergo <wamberg@protonmail.com>
2019-10-28 09:27:27 +0100

    Create konstanter.py

---
+++ konstanter.py
@@ -0,0 +1,17 @@
+AU = 149597870700. # Astronomical Unit
+year = 365.25 * 24 * 3600 # one year in seconds
+mp = 1.6726219*10**-27 # proton mass [kg]
+mu = 2.3 * mp # avr. molecular weight
+sig_h2 = 2e-19; # cross-section of H2 [m^2]
+k = 1.38064852*10**-23 # Boltzmann constant
+
+sigma_sb = 5.670367e-08 # Stefan-Boltzmann constant
+gas_const = 8.3145 # ideal gas constant R
+
+Msun = 1.9891*10**30 # one solar mass in kg
+M_centralstar = 0.5 * Msun # mass of central star [kg]
+G = 6.67408*10**-11 # gravitational constant
+
+delta = 0.8 # Power law index of surface density
+
+rho_s = 1600 # material density [kg/m^3]
